6b8d0f1e0ec3a4380729a8681ec0715e242db7c1
ori rinat <oririnat333@gmail.com>
2019-01-21 19:05:37 +0200

    upload

--- Victim copy/screenshot_capture.h
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
-#include <unistd.h>
-
-#define MAX_DISPLAYS_NUM 32
-#define SCREENSHOT_IMAGE_NAME "abc.jpg"
-
-typedef enum {CAPTURE_SUCCESSFULLY, CAPTURE_FAILED} SCREENSHOT_STATUS;
-SCREENSHOT_STATUS capture_screenshot();
